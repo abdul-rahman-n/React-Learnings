7db33f2b46b3c99e8b19407f9d6d92d52788b95d
Abdul Rahman <arahman@waymore.io>
2024-08-09 20:06:28 +0530

    Episode 4

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = 500 + 500;
+const Header = () => {
+  return (
+    <div className="head">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-food-logo-png-image_5687686.png"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const heading2 = <h2 className="head">{elem}</h2>;
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{background: "#f0f0f0"}}>
+      <img className="res-logo"
+        src="https://th-i.thgim.com/public/life-and-style/food/article23513530.ece/alternates/FREE_1200/MAMP13POURINGSALNA"
+        alt="res-logo"
+      />
+      <h3>Bismillah Hotel</h3>
+      <h4>Parotta,Kothu,Biriyani</h4>
+      <h4>5 stars</h4>
+      <h4>23 minutes</h4>
+    </div>
+  );
+};
 
-const Heading = () => <h1 className="head">heading Component</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div classNamw="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-const FirstComponent = () => (
-  <div>
-    {heading2}
-    <Heading />
-    <h1>First Component</h1>
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+    <Body />
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FirstComponent />);
+root.render(<AppLayout />);

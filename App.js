cb16700c009b20d14a87465a00964c1af15d19a9
Abdul Rahman <arahman@waymore.io>
2024-08-16 19:25:18 +0530

    Episode 4

--- App.js
+++ App.js
@@ -42,7 +42,23 @@ const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
-      <div classNamw="res-container">
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>

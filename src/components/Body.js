3fa9f48e7c20f6d7470352a571160aa7d2733950
Abdul Rahman <arahman@waymore.io>
2024-08-21 21:57:49 +0530

    Episode 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,40 +3,13 @@ import resList from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  const [list,setList] = useState([
-    {
-      info: {
-        id: "485445",
-        name: "Chinese Wok",
-        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-        costForTwo: "₹250 for two",
-        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-        avgRating: 3.9,
-        sla: {
-          slaString: "30-35 mins",
-        },
-      },
-    },
-    {
-      info: {
-        id: "485446",
-        name: "Bismillah",
-        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-        costForTwo: "₹250 for two",
-        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-        avgRating: 4.5,
-        sla: {
-          slaString: "30-35 mins",
-        },
-      },
-    },
-  ]);
+  const [list,setList] = useState(resList);
   return (
     <div className="body">
       <button
         className="filter-btn"
         onClick={() => {
-          list2 = list.filter((res) => res.info.avgRating >= 4.5);
+          list2 = list.filter((res) => res.info.avgRating > 4.5);
           setList(list2);
         }}
       >
